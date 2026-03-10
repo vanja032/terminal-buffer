7737eed886a566e8318bf90f20d64d6682b8b955
Vanja Sretenović <sretenovic.vanja32@gmail.com>
2026-03-10 17:57:36 +0100

    Add core text primitives and tests

---
+++ src/main/kotlin/terminalbuffer/model/TerminalColor.kt
@@ -0,0 +1,23 @@
+package com.vanjasretenovic.terminalbuffer.model
+
+enum class TerminalColor {
+    DEFAULT,
+
+    BLACK,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    WHITE,
+
+    BRIGHT_BLACK,
+    BRIGHT_RED,
+    BRIGHT_GREEN,
+    BRIGHT_YELLOW,
+    BRIGHT_BLUE,
+    BRIGHT_MAGENTA,
+    BRIGHT_CYAN,
+    BRIGHT_WHITE
+}
